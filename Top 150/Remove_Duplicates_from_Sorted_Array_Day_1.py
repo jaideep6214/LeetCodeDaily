0ab16f7899e1c8135c889371a36dd902724df2e3
jaideep6214 <43785047+jaideep6214@users.noreply.github.com>
2025-01-01 09:50:55 +0530

    Add files via upload

---
+++ Top 150/Remove_Duplicates_from_Sorted_Array_Day_1.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k = 0
+        for i in range (len(nums)):
+            if i == 0:
+                k+=1
+                continue
+            if nums[i]>nums[i-1]:
+                nums[k]= nums[i]
+                k+=1
+        return k
+        
