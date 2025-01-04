5a99cf4ea590511c490d81cc3870dd1627723ba1
jaideep6214 <43785047+jaideep6214@users.noreply.github.com>
2025-01-04 18:51:28 +0530

    Create Rotate_Array_Day_4.py

---
+++ Top 150/Rotate_Array_Day_4.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        k = k % length
+        nums[:] = nums[length-k:] + nums[:length-k]
+        return nums
+
+            
