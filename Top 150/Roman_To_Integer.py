1049a0ffe7228a1c8acb9d05d150e0c2580c60db
jaideep6214 <43785047+jaideep6214@users.noreply.github.com>
2025-01-12 14:31:26 +0530

    Create Roman_To_Integer.py

---
+++ Top 150/Roman_To_Integer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_value = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        number = 0
+        n = len(s)
+        for i in range(n):
+            if i < n-1 and roman_value[s[i]]<roman_value[s[i+1]]:
+                number-=roman_value[s[i]]
+            else:
+                number+=roman_value[s[i]]
+
+        return number
+
+            
+           
